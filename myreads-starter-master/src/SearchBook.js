45bc504dca39dc5a1d74ab0ee7cfaa0b59f2439d
Matija Galina <matija@krakensystems.co>
2018-07-10 15:54:56 +0200

    handleInput in search

--- myreads-starter-master/src/SearchBook.js
+++ myreads-starter-master/src/SearchBook.js
@@ -2,26 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 class SearchBook extends Component {
+
+  state = {
+    query: ''
+  };
+
+  handleInput(value) {
+    this.setState({
+      query: value.trim()
+    });
+  }
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/" className="close-search" >Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.handleInput(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {
+
+            }
+          </ol>
         </div>
       </div>
     )
