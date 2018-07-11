c02e6a5763c4ab12daad06dd9b065231481ae9bd
Matija Galina <matija@krakensystems.co>
2018-07-11 19:45:26 +0200

    sending state

--- myreads-starter-master/src/App.js
+++ myreads-starter-master/src/App.js
@@ -1,30 +1,42 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBook from './SearchBook'
-// import Book from './Book'
 import { Route, Link } from 'react-router-dom'
 import BookShelf from './BookShelf';
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    bookFiles: []
   }
 
   saveShelfChange(bookId, shelf) {
     console.log(bookId);
     console.log(shelf);
     this.setState(state => ({
-      books : state.books.concat([{
+      books: state.books.concat([{
         book: bookId,
         shelf: shelf
       }])
     }))
   }
 
+  getBooks() {
+    this.state.books.map(item => (
+      BooksAPI.get(item.book).then(data => this.setState(state => ({
+        bookFiles: state.bookFiles.concat([data])
+      })))
+    ))
+  }
+
+  componentDidMount() {
+    this.getBooks()
+  }
+
+
   render() {
 
-    const shelfTitles = ['Currently Reading', 'Want to Read', 'Read']
     // BooksAPI.getAll().then(data => console.log(data))
     // BooksAPI.update(this.book, 'read').then(data => console.log(data))
 
@@ -35,18 +47,39 @@ class BooksApp extends React.Component {
             shelfUpdate={(bookId, shelf) => this.saveShelfChange(bookId, shelf)}
           />
         )} />
-        <Route exact path='/' render={({history}) => (
+        <Route exact path='/' render={({ history }) => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
               <div>
-                {
-                  shelfTitles.map((title) => {
-                    <BookShelf shelfName={title} />
-                  })
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {/* {
+                        this.state.bookFiles.map((item, index) => (
+                          <Book key={index} book={item} />
+                        ))
+                      } */}
+                    </ol>
+                  </div>
+                </div>
+                <BookShelf
+                title="Want to Read"
+                books = {
+                  this.state.books.filter(item => (item.shelf === 'wantToRead' ))
                 }
+                />
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+
+                    </ol>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="open-search">
--- myreads-starter-master/src/BookShelf.js
+++ myreads-starter-master/src/BookShelf.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-// import Book from './Book'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
 
 class BookShelf extends Component {
+
+  books = [];
+
+  componentDidMount() {
+    this.props.books.map(item => (
+      BooksAPI.get(item.book).then(data => this.books.push(data))
+    ))
+  }
+
   render() {
+    console.log(this.books)
+
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+        <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
+            {
+              this.books.map((item, index) => (
+                <Book key={index} book={item} />
+              ))
+            }
           </ol>
         </div>
       </div>
