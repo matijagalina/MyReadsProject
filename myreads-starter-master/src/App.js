feee0a429252dd40a16114d2c88f062a05b61f27
Matija Galina <matija@krakensystems.co>
2018-07-14 11:12:32 +0200

    lint

--- myreads-starter-master/src/App.js
+++ myreads-starter-master/src/App.js
@@ -17,9 +17,8 @@ class BooksApp extends React.Component {
   }
 
   handleShelfChange(book, shelf) {
-    if (this.state.books) {
+    if (this.state.books.length > 0) {
       BooksAPI.update(book,shelf).then(() => {
-        book.shelf = shelf;
         this.setState(state => ({
           books: state.books.filter(item => item.id !== book.id).concat([ book ])
         }))
