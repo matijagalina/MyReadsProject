d1c4ad44029328bf48eda1bd44545d88ec445e7a
Matija Galina <matija@krakensystems.co>
2018-07-17 08:49:50 +0200

    authors joined with commas

--- myreads-starter-master/src/Book.js
+++ myreads-starter-master/src/Book.js
@@ -40,7 +40,7 @@ class Book extends Component {
           </div>
           <div className="book-title">{this.props.book.title}</div>
           <div className="book-authors">
-            {this.props.book.authors}
+          {this.props.book.authors ? this.props.book.authors.join(', ') : ''}
           </div>
         </div>
       </li>
